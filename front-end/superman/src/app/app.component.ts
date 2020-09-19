d7c6fa988d29e3cbfa40e56b35e418a7652f0964
Majid Shaikh <majidshaikh16@gmail.com>
2020-09-19 18:53:42 +0530

    updated the front end message

--- front-end/superman/src/app/app.component.ts
+++ front-end/superman/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Superman for you!';
+  title = 'Superman for you! Version 1.0';
 }
