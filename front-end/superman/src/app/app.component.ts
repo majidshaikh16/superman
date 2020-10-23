4877c233d59f4df80d9316b18d5d5289541e6782
Majid Shaikh <majidshaikh16@gmail.com>
2020-10-23 11:48:06 +0530

    updated version

--- front-end/superman/src/app/app.component.ts
+++ front-end/superman/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Superman for you! Version 2.4';
+  title = 'Superman for you! Version 2.5';
 }
